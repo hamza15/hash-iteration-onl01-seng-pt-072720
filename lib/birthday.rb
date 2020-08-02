af43d78c86147ebe0cb5b0a85075e24119203b2f
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-02 23:15:28 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,7 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kid, age|
-    puts ""
+    puts "Happy Birthday #{kid}! You are now #{age} years old!"
 end
 
 
